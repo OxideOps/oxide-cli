8d8e86aa6e15e8e206a7b9f4efcb2b0d1409c2c3
Dillon-Roller <dillonroller.18@gmail.com>
2023-09-06 23:51:42 -0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,107 @@
-use std::process::Command;
+use std::process::Command as OsCommand;
+use thiserror::Error;
 
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
+#[derive(Debug, Error)]
+enum OxideError {
+    #[error("Invalid or insufficient arguments. Usage: oxide [run|build] <PACKAGE> | [deploy] | [setup]")]
+    InvalidArgs,
 
-    if args.len() < 3 {
-        println!("Usage: oxide [run|build] <PACKAGE>");
-        return;
+    #[error("The '{0:?}' action requires a package name.")]
+    MissingPackageName(Action),
+
+    #[error("{0}")]
+    ExecutionFailure(String),
+
+    #[error("{0}")]
+    CommandFailure(String),
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Action {
+    Run,
+    Build,
+    Deploy,
+    Setup,
+}
+
+struct Command {
+    action: Action,
+    args: Vec<String>,
+}
+
+impl Command {
+    fn from_args(args: &[String]) -> Result<Command, OxideError> {
+        if args.len() < 2 {
+            return Err(OxideError::InvalidArgs);
+        }
+
+        let action = match args[1].as_str() {
+            "run" => Action::Run,
+            "build" => Action::Build,
+            "deploy" => Action::Deploy,
+            "setup" => Action::Setup,
+            _ => return Err(OxideError::InvalidArgs),
+        };
+
+        Ok(Command {
+            action,
+            args: args[2..].to_vec(),
+        })
     }
 
-    let command = &args[1];
-    let package = &args[2];
-
-    match command.as_str() {
-        "run" => run_package(package),
-        "build" => build_package(package),
-        _ => {
-            println!("Invalid command. Usage: oxide [run|build] <PACKAGE>");
+    fn execute(&self) -> Result<(), OxideError> {
+        match &self.action {
+            Action::Run | Action::Build => {
+                if let Some(package) = self.args.get(0) {
+                    run_or_build(&self.action, package)
+                } else {
+                    Err(OxideError::MissingPackageName(self.action))
+                }
+            }
+            Action::Deploy => fly_deploy(),
+            Action::Setup => setup(),
         }
     }
 }
 
-fn run_package(package: &str) {
-    let mut cmd = Command::new("cargo");
-    cmd.args(["run", "-p", package]);
-    cmd.spawn().expect("Failed to run the package");
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if let Err(error) = Command::from_args(&args).and_then(|command| command.execute()) {
+        eprintln!("{}", error);
+        std::process::exit(1);
+    }
 }
 
-fn build_package(package: &str) {
-    let mut cmd = Command::new("cargo");
-    cmd.args(["build", "-p", package]);
-    cmd.spawn().expect("Failed to build the package");
+fn run_command(command: &str, args: &[&str], execution_msg: &str, failure_msg: &str) -> Result<(), OxideError> {
+    let status = OsCommand::new(command)
+        .args(args)
+        .status()
+        .map_err(|_| OxideError::ExecutionFailure(execution_msg.to_string()))?;
+
+    if status.success() {
+        Ok(())
+    } else {
+        Err(OxideError::CommandFailure(failure_msg.to_string()))
+    }
 }
 
-fn fly_deploy() {
-    todo!("Implement fly deploy in CLI tool")
+fn run_or_build(action: &Action, package: &str) -> Result<(), OxideError> {
+    let action_str = match action {
+        Action::Run => "run",
+        Action::Build => "build",
+        _ => unreachable!(),
+    };
+    
+    run_command("cargo", &[action_str, "-p", package], 
+                &format!("Failed to execute cargo {} command", action_str), 
+                &format!("Cargo {} for {} failed", action_str, package))
 }
 
+fn fly_deploy() -> Result<(), OxideError> {
+    run_command("flyctl", &["deploy"], "Failed to execute fly deploy command", "Fly deploy failed")
+}
+
+fn setup() -> Result<(), OxideError> {
+    run_command("./setup.sh", &[], "Failed to execute setup. Ensure that you are at the root of the project", "Setup failed")
+}
